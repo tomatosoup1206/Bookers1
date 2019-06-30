cebc7474fc423a37fec88c6bbfc92aa04ff823a2
tomatosoup1206 <kodama.tokiya.infratop@gmail.com>
2019-07-01 04:49:50 +0900

    finish

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,51 @@
+class BooksController < ApplicationController
+  def top
+  end
+
+  def index
+  	@books = Book.all
+  	@book = Book.new
+  end
+
+  def create
+  	book = Book.new(book_params)
+  	if book.save
+  	  redirect_to book_path(book.id), notice: "Book was successfully created"
+    else
+      render action: :index
+    end
+  end
+
+  def edit
+  	@book = Book.find(params[:id])
+  end
+
+  def update
+  	book = Book.find(params[:id])
+  	if book.update(book_params)
+  	  redirect_to book_path(book.id), notice: "Book was successfully updated"
+    else
+      render action: :index
+    end
+  end
+
+  def show
+  	@book = Book.find(params[:id])
+  end
+
+  def destroy
+  	book = Book.find(params[:id])
+  	if book.destroy
+  	  redirect_to books_path, notice: "Book was successfully destroyed"
+    else
+      render action: :index
+    end
+  end
+
+  private
+  def book_params
+  	params.require(:book).permit(:title,:body)
+  end
+
+
+end
